7410e697ce258a3605816010cd5ae34c353e51ff
Frida Jansson <frja2400@student.miun.se>
2025-04-26 17:41:14 +0200

    Skapade en konverterare för C - F. Adderade egenskaper och metoder till min klass. Importerade FormModule. Använda tvåvägs data-binding med ngModel.

--- src/app/converter/converter.component.ts
+++ src/app/converter/converter.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  imports: [FormsModule], //Importerar FormsModule för formulär
   templateUrl: './converter.component.html',
   styleUrl: './converter.component.css'
 })
 export class ConverterComponent {
+  //Egenskaper av klassen ConverterComponent
+  celsiusValue: number = 0;
+  fahrenheitValue: number = 0;
+  result: number = 0;
 
+  //Metoder
+  convertToFahrenheit(): void {
+    this.result = (this.celsiusValue * 9 / 5) + 32;
+  }
+
+  convertToCelsius(): void {
+    this.result = (this.fahrenheitValue - 32) * 5 / 9;
+  }
 }
